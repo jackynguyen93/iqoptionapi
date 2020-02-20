4358b3bbdcf6220bcb7791a59b00f08767992043
Nguyen <jackynguyen93@gmail.com>
2020-02-20 18:36:32 +0700

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""The python wrapper for IQ Option API package setup."""
+from setuptools import (setup, find_packages)
+
+setup(
+    name="iqoptionapi",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=["pylint","requests","websocket-client==0.56"],
+    include_package_data = True,
+    description="Best IQ Option API for python",
+    long_description="Best IQ Option API for python",
+    url="https://github.com/jackynguyen93/iqoptionapi",
+    author="Jacky Nguyen",
+    author_email="jackynguyen93@gmail.com",
+    zip_safe=False
+)
